29ab36668f873bf3ab4a7b7c4af6140fb3fe21d1
James Moro <james.moro.11@gmail.com>
2021-06-18 18:23:18 -0700

    added timing

--- Euler16.py
+++ Euler16.py
@@ -3,7 +3,9 @@
 
 What is the sum of the digits of the number 2**1000?
 """
+import time
 
+start = time.time()
+print(sum(int(digit) for digit in str(2 ** 1000)))
 
-
-print(sum(int(digit) for digit in str(2**1000)))
+print(f"solution took {time.time() - start} seconds")
